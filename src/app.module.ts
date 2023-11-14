1cd4040c4b864b378bf5c194db61dcf27c1ab60d
Antoine Dabonneville <antoine.dabonneville@sogeti.com>
2023-11-14 14:41:07 +0100

    [CORE] init project, create module, controller, entities and db connection

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { ReportsController } from './reports/reports.controller';
+import { ReportsService } from './reports/reports.service';
+import { UsersService } from './users/users.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController, ReportsController],
+  providers: [AppService, ReportsService, UsersService],
+})
+export class AppModule {}
