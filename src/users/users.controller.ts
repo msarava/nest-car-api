7a39ff98c27edd1edaba2767da62eb3ac85f785a
Antoine Dabonneville <antoine.dabonneville@sogeti.com>
2023-11-15 11:35:27 +0100

    [USER API] complete CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -8,6 +19,31 @@ export class UsersController {
 
   @Post('/signup')
   CreateUser(@Body() body: CreateUserDTO) {
-    this.userService.create(body.email, body.password);
+    return this.userService.create(body.email, body.password);
+  }
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const userId = parseInt(id, 10);
+    const user = await this.userService.findOne(userId);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const userId = parseInt(id, 10);
+
+    return this.userService.update(userId, body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id, 10));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -25,13 +25,21 @@ export class UsersService {
     const userToUpdate = await this.findOne(id);
 
     if (!userToUpdate) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
 
     //Assign properties in 'attribute' to userToUpdate
     Object.assign(userToUpdate, attribute);
-    this.repo.save(userToUpdate);
+    return this.repo.save(userToUpdate);
   }
 
-  //TODO remove() {}
+  async remove(id: number) {
+    const userToremove = await this.findOne(id);
+
+    if (!userToremove) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.repo.remove(userToremove);
+  }
 }
